b80577e163bdeb41e85858d78ab7e3ae17bb45df
Daniel <danielbarbosa27@icloud.com>
2026-08-26 15:43:20 -0300

    Funções iniciais para cada metodo

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
+
+REMOVE = threads mandelbrot_dsob_openmp.pgm mandelbrot_dsob_pthreads1.pgm mandelbrot_dsob_serial.pgm
+
 all:
-	gcc main.c -o threads
+	gcc main.c -o threads -lpthread
 	./threads 1 1 1 1
 
 compile:
 	gcc main.c -o threads
 
 clean:
-	rm -f threads
+	rm -f $(REMOVE)
--- main.c
+++ main.c
@@ -3,13 +3,33 @@
 #include <pthread.h>
 #include <omp.h>
 
-void calculo_mandelbrot(float largura, float altura, long max_iter, long threads){
+float calculo_mandelbrot(float largura, float altura, long max_iter, long threads){
     printf("%f %f %ld %ld\n", largura, altura, max_iter, threads);
+    return 2.0;
+}
+
+void processo_pthread(float basico){
+    FILE *file = fopen("mandelbrot_dsob_pthreads1.pgm", "w");
+
+    fprintf(file, "%f", basico);
+    return;
+}
+
+void processo_openmp(float basico){
+    FILE *file = fopen("mandelbrot_dsob_openmp.pgm", "w");
+    
+    fprintf(file, "%f", basico);
+    return;
+}
+
+void processo_serial(float basico){
+    FILE *file = fopen("mandelbrot_dsob_serial.pgm", "w");
+
+    fprintf(file, "%f", basico);
     return;
 }
 
 int main(int argc, char **argv){
-
     if (argc < 5){
         printf("Menos argumentos do que requisitado");
         return 1;
@@ -21,6 +41,11 @@ int main(int argc, char **argv){
     long max_iter = strtol(argv[3],&receber, 10);
     long threads = strtol(argv[4],&receber, 10);
 
-    calculo_mandelbrot(largura, altura, max_iter, threads);
+    float basico = calculo_mandelbrot(largura, altura, max_iter, threads);
+
+    processo_serial(basico);
+    processo_openmp(basico);
+    processo_pthread(basico);
+
     return 0;
 }
